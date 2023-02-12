701411bf05b6b0a0d3b1a181e9def4a0726c9c2e
2swap <mchontz10@gmail.com>
2023-02-11 19:24:09 -0800

    more connect 4 gamemodes

--- connect4_server.js
+++ connect4_server.js
@@ -1,6 +1,8 @@
-height = 6;                     // number of rows
-width = 7;                      // number of columns
-currentPlayer = 1;              // Whose turn is it - first player is 1 and second is 2. 0 signifies empty square
+var height = 6;                     // number of rows
+var width = 7;                      // number of columns
+var currentPlayer = 1;              // Whose turn is it - first player is 1 and second is 2. 0 signifies empty square
+
+var pushupType = 1; //0=normal 1=pushup 2=falloutoftop
 
 onClientClick = function(boardList, data){
 	// don't permit data with absent coordinates
@@ -21,14 +23,30 @@ onClientClick = function(boardList, data){
 	if(x >= width || x < 0)
 		return false;
 
-	for(var y = 0; y < height; y++){
-        if(boardList[x][y] == 0){
-            boardList[x][y] = currentPlayer;
-            break;
-        }
-        if(y == height-1)
+    if(pushup == 0)
+    	for(var y = 0; y < height; y++){
+            if(boardList[x][y] == 0){
+                boardList[x][y] = currentPlayer;
+                break;
+            }
+            if(y == height-1)
+                return false;
+	    }
+    else if (pushup==1){
+    	for(var y = height-1; y > 0; y--){
+            boardList[x][y] = boardList[x][y-1];
+	    }
+        boardList[x][y] = currentPlayer;
+    }
+    else if(pushup == 2){
+        if(boardList[x][height-1] != 0)
             return false;
-	}
+    	for(var y = height-1; y > 0; y--){
+            boardList[x][y] = boardList[x][y-1];
+	    }
+        boardList[x][y] = currentPlayer;
+    }
+
 
     currentPlayer = currentPlayer == 1?2:1;
 
